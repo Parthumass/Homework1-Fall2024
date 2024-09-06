2111abbc8a9c3c7798df85746251196c51b7ebdd
zibin <chenzibin2017@outlook.com>
2024-09-06 14:15:20 -0400

    Question 1 template

---
+++ question1.py
@@ -0,0 +1,20 @@
+"""
+ECE241 Fall 2024 - Homework1 Question1
+"""
+
+# Use the following constants if you need
+below_20 = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve',
+            'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+thousands = ['', 'thousand', 'million']
+
+def stringify(number):
+    pass
+
+if __name__ == "__main__":
+    print(stringify(1))
+    print(stringify(10))
+    print(stringify(2024))
+    print(stringify(20242024))
+    print(stringify(202420242024))
+
