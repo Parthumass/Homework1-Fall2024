55434160d0ab241e77b6fb282f9f44013d0f98b0
zibin <chenzibin2017@outlook.com>
2024-09-06 14:17:41 -0400

    Excepted Output

--- question1.py
+++ question1.py
@@ -8,13 +8,13 @@ below_20 = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 
 tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
 thousands = ['', 'thousand', 'million']
 
+
 def stringify(number):
     pass
 
 if __name__ == "__main__":
-    print(stringify(1))
-    print(stringify(10))
-    print(stringify(2024))
-    print(stringify(20242024))
-    print(stringify(202420242024))
-
+    print(stringify(1))         # Excepted: one
+    print(stringify(10))        # Excepted: ten
+    print(stringify(2024))      # Excepted: two thousand, twenty four
+    print(stringify(20242024))  # Excepted: twenty million, two hundred and forty two thousand, twenty four
+    print(stringify(202420242024)) # Excepted: four hundred and twenty million, two hundred and forty two thousand, twenty four
